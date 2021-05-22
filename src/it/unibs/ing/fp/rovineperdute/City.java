15dbfef90483a53a896a242c04140ac1e1e1e828
Mattia Visini <mattiavisini95@gmail.com>
2021-05-22 17:05:51 +0200

    city, vehicle

---
+++ src/it/unibs/ing/fp/rovineperdute/City.java
@@ -0,0 +1,10 @@
+package it.unibs.ing.fp.rovineperdute;
+
+public class City {
+
+    private int id;
+    private String name;
+    private Coordinate coordinate;
+
+
+}
---
+++ src/it/unibs/ing/fp/rovineperdute/Vehicle.java
@@ -0,0 +1,26 @@
+package it.unibs.ing.fp.rovineperdute;
+
+public class Vehicle {
+
+    private String team_name;
+    private int vehicle_type;
+
+    /**
+     * Costructor of a vehicle
+     * @param team_name Vehicle team name
+     * @param vehicle_type Integer which indicates the type of vehicle and how it consumes fuel
+     */
+    public Vehicle(String team_name, int vehicle_type) {
+        this.team_name = team_name;
+        this.vehicle_type = vehicle_type;
+    }
+
+    /**
+     * Method that return the team name
+     * @return Return team name
+     */
+    public String getTeam_name() {
+        return team_name;
+    }
+
+}
