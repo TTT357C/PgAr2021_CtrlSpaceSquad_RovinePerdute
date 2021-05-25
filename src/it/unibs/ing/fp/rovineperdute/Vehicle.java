d80914a8b2bbbbad4722c754eea887b2eba20178
Mattia Visini <mattiavisini95@gmail.com>
2021-05-25 16:56:53 +0200

    comments

--- src/it/unibs/ing/fp/rovineperdute/Vehicle.java
+++ src/it/unibs/ing/fp/rovineperdute/Vehicle.java
@@ -9,6 +9,8 @@ public class Vehicle {
      * Constructor method of a vehicle
      * @param team_name Vehicle team name
      * @param vehicle_type Integer which indicates the type of vehicle and how it consumes fuel
+     *                     - 0 for Team Tonatiuh, zero fuel on difference in altitude
+     *                     - 1 for Team Metztli, zero fuel between city on the same altitude
      */
     public Vehicle(String team_name, int vehicle_type) {
         this.team_name = team_name;
@@ -21,6 +23,18 @@ public class Vehicle {
      */
     public String getTeam_name() {
         return team_name;
+    }
+
+    public double calculateFuel(){
+        //calculation fuel for team tonatiuh
+        if(vehicle_type == 0){
+
+        }
+
+        //calculation fuel for team Metztli
+        if(vehicle_type == 1){
+
+        }
     }
 
 }
