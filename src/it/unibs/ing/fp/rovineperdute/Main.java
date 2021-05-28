b89a65f002b7f1509aa361b2386e26c7118b72a7
Thomas <thomas.causetti@gmail.com>
2021-05-28 16:26:15 +0200

    Main V 2.0

--- src/it/unibs/ing/fp/rovineperdute/Main.java
+++ src/it/unibs/ing/fp/rovineperdute/Main.java
@@ -4,9 +4,12 @@ import it.unibs.ing.fp.altro.ksp.Yen;
 import it.unibs.ing.fp.altro.util.Path;
 import it.unibs.ing.fp.mylib.InputDati;
 import it.unibs.ing.fp.pathfinding.*;
+
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -22,6 +25,11 @@ public class Main {
             " |    |   (  <_> )   /|  |   |  \\  ___/   |    |   \\  ___/|  | \\/ /_/ |  |  /|  | \\  ___/ \n" +
             " |____|_  /\\____/ \\_/ |__|___|  /\\___  >  |____|    \\___  >__|  \\____ |____/ |__|  \\___  >\n" +
             "        \\/                    \\/     \\/                 \\/           \\/                \\/ \n";
+    public static final String MENU2 =  "  _____________________________________________\n"+
+                                        " |                                             |\n"+
+                                        " |  1- Fast Execution (A*)                     |\n"+
+                                        " |  2- Slow Execution (A* and Yen)             |\n"+
+                                        " |_____________________________________________|\n";
     public static final String TEAM_NAME_1 = "Tonathiu";
     public static final String TEAM_NAME_2 = "Metztli";
     //========================================================================================================
@@ -35,10 +43,25 @@ public class Main {
 
         //========================================================================================================
         //Number of vertices
-        int vertices=200;
+        int vertices=2000;
 
         //New instance of ReadXML
         ReadXML read = new ReadXML();
+        //========================================================================================================
+
+        //Menu (File chooser)
+        //========================================================================================================
+        File folder = new File("test_file");
+        File[] listOfFiles = folder.listFiles();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                System.out.println(" "+(i+1)+"-" + listOfFiles[i].getName());
+            }
+        }
+
+        int index_t = InputDati.leggiIntero(" Enter the number for choosing the file: ");
+        String file_name = listOfFiles[index_t-1].getName();
         //========================================================================================================
 
 
@@ -51,19 +74,25 @@ public class Main {
 
         //Read from file
         //========================================================================================================
-        read.readCities(cities,"test_file/PgAr_Map_"+vertices+".xml");
-        read.readCities(cities_temp,"test_file/PgAr_Map_"+vertices+".xml");
+        read.readCities(cities,"test_file/"+file_name);
+        read.readCities(cities_temp,"test_file/"+file_name);
+        //========================================================================================================
+
+        //Vehicle
+        //========================================================================================================
+        Vehicle team=new Vehicle(TEAM_NAME_1,0);
+        Vehicle team2=new Vehicle(TEAM_NAME_2,1);
         //========================================================================================================
 
 
         //Weight Calculation
         //========================================================================================================
         for (City city:cities) {
-            city.calculateLink(new Vehicle(TEAM_NAME_1,0),cities);
+            city.calculateLink(team,cities);
         }
 
         for (City city:cities_temp) {
-            city.calculateLink(new Vehicle(TEAM_NAME_2,1),cities_temp);
+            city.calculateLink(team2,cities_temp);
         }
         //========================================================================================================
 
@@ -71,80 +100,64 @@ public class Main {
         //Menu
         //========================================================================================================
         System.out.println(MENU);
+        System.out.println(MENU2);
         //variabile for time calculation
         long timeStartTot=0;
 
+        ArrayList<Integer> ids=new ArrayList<>();
+        ArrayList<Integer> ids2=new ArrayList<>();
+        double fuel=0;
+        double fuel2=0;
 
         //========================================================================================================
-        int scelta=InputDati.leggiIntero("-->",0,2);
+        int scelta=InputDati.leggiIntero("-->",1,2);
         switch (scelta){
-            //========================================================================================================
-            case 0:
-                //Exit
-            break;
             //========================================================================================================
             case 1:
                 timeStartTot = System.currentTimeMillis();
                 //Fast Calculation
-                aStarInitializer(vertices,cities);
-                aStarInitializer(vertices,cities_temp);
+
+                fuel=aStarInitializer(vertices,cities,ids);
+                fuel2=aStarInitializer(vertices,cities_temp,ids2);
             break;
             //========================================================================================================
             case 2:
                 timeStartTot = System.currentTimeMillis();
                 //Slow Calculation
-                aStarInitializer(vertices,cities);
-                aStarInitializer(vertices,cities_temp);
+                fuel=aStarInitializer(vertices,cities,ids);
+                fuel2=aStarInitializer(vertices,cities_temp,ids2);
                 yenPreInitializer(vertices, cities);
                 yenPreInitializer(vertices, cities_temp);
             break;
             //========================================================================================================
         }
 
+        //File Exporter
+        //========================================================================================================
+        WriteXML writeXML = new WriteXML();
+        ArrayList<City> output=new ArrayList<>();
+        ArrayList<City> output2=new ArrayList<>();
+        for (int index:ids) {
+            output.add(cities.get(index));
+        }
+        for (int index:ids2) {
+            output2.add(cities_temp.get(index));
+        }
+        team.setTouched_cities(output);
+        team2.setTouched_cities(output2);
+        team.setFuel(fuel);
+        team2.setFuel(fuel2);
+        writeXML.writeXML(team,team2);
+        System.out.println(" Successfully wrote to the file.");
+        //========================================================================================================
 
         //========================================================================================================
         //End of the execution with time
         System.out.println(LINE);
         long timeFinishTot = System.currentTimeMillis();
         System.out.println(" "+"Total operation took " + (timeFinishTot - timeStartTot) / 1000.0 + " seconds.");
-        //========================================================================================================
-
-
-        //matrix
-        /*
-        if(vertices<250) {
-            double mat[][] = new double[vertices][vertices];
-
-            for (int i = 0; i < cities.size(); i++) {
-                for (int j = 0; j < cities.get(i).getNeighbors().size(); j++) {
-                    mat[i][cities.get(i).getNeighbors().get(j).city_id] = cities.get(i).getNeighbors().get(j).weight;
-                }
-            }
-
-
-
-            try {
-                FileWriter myWriter = new FileWriter("Matrice.txt");
-                for (int i = 0; i < vertices; i++) {
-                    for (int j = 0; j < vertices; j++) {
-                        myWriter.write(((int) mat[i][j]) + ",");
-
-                    }
-                    //System.out.println("i: "+i+"\n");
-                    myWriter.write("\n");
-                }
-                myWriter.close();
-                System.out.println("Successfully wrote to the file.");
-            } catch (IOException e) {
-                System.out.println("An error occurred.");
-                e.printStackTrace();
-            }
-        }
-        */
 
         //================================================================
-
-        //System.out.println(cities);
     }
 
     private static void yenPreInitializer(int vertices, ArrayList<City> cities) {
@@ -160,7 +173,7 @@ public class Main {
 
             }
             myWriter.close();
-            System.out.println(" "+"Successfully wrote to the file.");
+
         } catch (IOException e) {
             System.out.println(" "+"An error occurred.");
             e.printStackTrace();
@@ -173,7 +186,9 @@ public class Main {
         graphFilename = "Matrice2.txt";
         sourceNode = "0";
         targetNode = (vertices -1)+"";
-        K = 2;
+        System.out.print(" "+"Computing the k shortest paths from [" + sourceNode + "] to [" + targetNode + "] ");
+        System.out.println("using Yen's algorithm.\n");
+        K = InputDati.leggiInteroConMinimo(" Enter the number of routes requested (k): ",1);
 
         System.out.println(LINE);
 
@@ -182,7 +197,7 @@ public class Main {
         System.out.println(LINE);
     }
 
-    private static void aStarInitializer(int vertices, ArrayList<City> cities) {
+    private static double aStarInitializer(int vertices, ArrayList<City> cities, ArrayList<Integer> ids) {
         for (int i = cities.size()-1; i >=0 ; i--) {
             cities.get(i).setH(i);
         }
@@ -198,7 +213,8 @@ public class Main {
         pathFinder.aStar(0, vertices - 1,cities);
         //System.out.print(".");
         //System.out.println("complete.");
-        ArrayList<Integer> ids = pathFinder.outputArrayGen(vertices - 1);
+
+        ids.addAll(pathFinder.outputArrayGen(vertices - 1));
         ProgressBar.value(7);
 
 
@@ -207,28 +223,25 @@ public class Main {
         double total_sum = pathFinder.sumFuel(ids);
 
         //System.out.println(total_sum);
-        System.out.println(" "+"n) cost: [path]");
-        System.out.print(" "+1 + ") " +total_sum+": ");
+        System.out.print(" "+1 + ") cost: " +total_sum+" - [path] ");
         pathFinder.viewPath2(ids);
 
 
         //Numero citta' toccate
         int number = pathFinder.getNumber_city();
-        System.out.println(" "+number);
+        System.out.println(" Number of cities: "+number);
         long timeFinish_a = System.currentTimeMillis();
         System.out.println(" "+"Operation took " + (timeFinish_a - timeStart_a) / 1000.0 + " seconds.");
         System.out.println(LINE);
+
+        return total_sum;
     }
 
     public static void yenInitializer(String graphFilename, String source, String target, int k) {
         /* Read graph from file */
-        System.out.print(" "+"Reading data from file... ");
         Graph graph = new Graph(graphFilename);
-        System.out.println("complete.");
 
         /* Compute the K shortest paths and record the completion time */
-        System.out.print(" "+"Computing the " + k + " shortest paths from [" + source + "] to [" + target + "] ");
-        System.out.println("using Yen's algorithm.\n");
         List<Path> ksp;
         long timeStart = System.currentTimeMillis();
         Yen yenAlgorithm = new Yen();
